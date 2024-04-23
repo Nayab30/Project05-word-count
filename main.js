692a199df8d2fe5c1c48af49fb666e4259c39899
[nayab aslam] <[aslamnayab56@gmail.com]>
2024-04-23 10:56:34 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+//This is line is called shebang
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the words"
+    }
+]);
+// use trim and split method  to remove white spaces in sentences
+// trim remove white space start and end only
+const word = answer.sentence.trim().split(" ");
+console.log(word);
+console.log(`Your sentence word count is  ${word.length}`);
